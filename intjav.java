912fe4f5b76cb7c073a4b2c843e2e0679b18652f
Gulshan Godsora <gulshangodsora008@gmail.com>
2024-11-01 20:57:59 +0530

    integer

--- intjav.java
+++ intjav.java
@@ -1,5 +1,9 @@
 public class intjav {
     public static void main(String args[]) {
-        
+    int num;
+
+    System.out.println("Enter the integer: ");
+    Scanner s = new Scanner(System.in);
+
     }
 }
