4de0f1d9ec2296a46eed5c8a33bb307a72eff617
Gulshan Godsora <gulshangodsora008@gmail.com>
2024-11-08 14:06:45 +0530

    diamond new

--- intjav.java
+++ intjav.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 public class intjav {
     public static void main(String args[]) {
-    int num;
+    int s;
 
     System.out.println("Enter the integer: ");
-    Scanner s = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     }
 }
